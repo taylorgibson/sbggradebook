34c4d4cd87ed0656a53429e914adc35214b091ab
taylorgibson <taylorgibson@gmail.com>
2017-01-19 15:34:45 -0500

    Update gradebookConfig.py

--- gradebookConfig.py
+++ gradebookConfig.py
@@ -1,3 +1,6 @@
-myCourse = ''
-TOKEN = ''
-PAGES = '2000'
+myCourse = '####'         #Canvas Course Number
+emailPassword = '####'    #Create an app-specific password for your school issued Gmail Account
+emailAddress = '####'     #Your School specific e-mail account
+courseName = 'Pre-Calculus w/ Advanced Topics'  #What you'd like your course to be called in reports
+TOKEN = '####'            #Create a token in Canvas to give script access to your grades
+PAGES = '2000'            #Keep a large number so script can pull in many scores at once
